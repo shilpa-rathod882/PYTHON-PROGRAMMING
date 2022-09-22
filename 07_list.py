1e4e9b80e4a41e6e7d3a79b010f483e61c5b1752
shilpa-rathod882 <shilpab882@gmail.com>
2022-09-22 13:33:29 +0530

    lst

---
+++ 07_list.py
@@ -0,0 +1,23 @@
+lst = []
+print(type(lst))
+
+lst2 = list()
+print(type(lst2))
+
+lst3 = [1,2,3]
+print(lst3)
+
+lst4 = ["code" , "complte", "shilpa"]
+print(lst4)
+
+data = [lst3, 4, 5, 7, lst4, True, False]
+print(data)
+
+print(data)
+print(data[0])
+print(data[-1])
+
+
+print(data[0:3])
+print(data[:-1])
+print(data[1:])
