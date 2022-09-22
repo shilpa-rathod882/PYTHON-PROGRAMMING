04edcdd9fe1dbbae70e123366e22b90950b410be
shilpa-rathod882 <shilpab882@gmail.com>
2022-09-22 13:25:49 +0530

    string methods

---
+++ 06_string_methods.py
@@ -0,0 +1,16 @@
+name = "codecomplete"
+
+print(name.capitalize())
+
+print(name.upper())
+
+print(name.swapcase)
+
+print(name.count("e"))
+
+print(name.replace("c","c"))
+
+print(name.find("p"))
+
+print(name.isalpha())
+
